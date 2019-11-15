4c702f8e08fa35dd1368cd40a802327f59bd62df
patha.krishna <patha.krishna@eagleview.com>
2019-11-15 11:20:25 +0530

    Math Library
    Signed-off-by: PKRISH-BLRM19 <patha.krishna@PKRISH-BLRM19.local>

---
+++ utils/MathLibrary.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"fmt"
+)
+
+//SumNumbers Sum of two Integers passed
+func SumNumbers(a int, b int) int {
+	fmt.Println("Numbers Provided are : ", a, b)
+	return a + b
+}
